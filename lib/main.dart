1826b1d6fc1020716903733a9ee79d44718217f8
jainhimker07 <jainhimker07@gmail.com>
2025-06-26 22:39:57 +0530

    Firebase integration

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,8 @@ import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform,
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
   );
   runApp(const PokerPalApp());
 }
